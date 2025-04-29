a03a4bb6fd87e9abc82ec890eea4d7bfbaf46f82
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-04-29 20:09:44 +0530

    Create Employee file in backend Model

---
+++ React and Entity Framework/backend/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace backend.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public String? Name { get; set; }
+        public String? Age { get; set; }
+        public int? IsActive { get; set; }
+
+    }
+}
